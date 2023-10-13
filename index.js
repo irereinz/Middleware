7babf68060c62418d8ca5d3f7a13b6da2454a8ce
Ireneus Widhi Arianto <ireniusreinz3@gmail.com>
2023-10-13 10:49:06 +0700

    adding authentication password

--- index.js
+++ index.js
@@ -4,14 +4,28 @@ const app = express();
 
 app.use((morgan('dev')));
 
+// app.use('/', (req,res,next) =>{
+//     console.log('middleware pertama')
+//     next()
+//     console.log('middleware pertama setelah next');
+// })
+// app.use('/halaman', (req,res,next) =>{
+//     console.log('middleware kedua')
+//     next();
+// })
+
 app.use('/', (req,res,next) =>{
-    console.log('middleware pertama')
-    next()
-    console.log('middleware pertama setelah next');
-})
-app.use('/halaman', (req,res,next) =>{
-    console.log('middleware kedua')
+    req.timeRequest = Date.now();
+    console.log(req.method, req.url)
     next();
+})
+
+app.use((req,res,next) => {
+    const { password } = req.query; 
+    if (password === 'ninjasaga') {
+        next();
+    }
+    res.send('cant access')
 })
 
 app.get('/', (req,res) => {
@@ -19,7 +33,12 @@ app.get('/', (req,res) => {
 })
 
 app.get('/halaman', (req,res) => {
+    console.log(req.timeRequest);
     res.send('Hello ini Halaman')
+})
+
+app.use((req,res) =>{
+    res.status(404).send('page not found')
 })
 
 app.listen(3000, ()=>{
